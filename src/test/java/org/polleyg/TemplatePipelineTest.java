33b3771332505450ed5ed80a8fd37926a9545546
AURORE <aurore.pauchet@decathlon.com>
2021-01-18 16:07:42 +0100

    first test

--- src/test/java/org/polleyg/TemplatePipelineTest.java
+++ src/test/java/org/polleyg/TemplatePipelineTest.java
@@ -1,5 +1,6 @@
 package org.polleyg;
 
+import com.google.api.services.bigquery.model.TableFieldSchema;
 import com.google.api.services.bigquery.model.TableRow;
 import org.apache.beam.sdk.transforms.DoFnTester;
 import org.junit.AfterClass;
@@ -35,19 +36,21 @@ public class TemplatePipelineTest {
 
         List<String> input = new ArrayList<>();
 
-        input.add("2018,8,13,Wikinews,English,Spanish football: Sevilla signs Aleix Vidal from FC Barcelona,12331");
+        input.add("2018,8,13,Wikinews,English,Spanish football: Sevilla signs Aleix Vidal from FC Barcelona,12331, 12331? 12331");
 
         List<TableRow> output = fnTester.processBundle(input);
 
         Assert.assertThat(output, is(not(empty())));
 
-        Assert.assertThat(output.get(0).get("year"), is(equalTo("2018")));
-        Assert.assertThat(output.get(0).get("month"), is(equalTo("8")));
-        Assert.assertThat(output.get(0).get("day"), is(equalTo("13")));
-        Assert.assertThat(output.get(0).get("wikimedia_project"), is(equalTo("Wikinews")));
-        Assert.assertThat(output.get(0).get("language"), is(equalTo("English")));
-        Assert.assertThat(output.get(0).get("title"), is(equalTo("Spanish football: Sevilla signs Aleix Vidal from FC Barcelona")));
-        Assert.assertThat(output.get(0).get("views"), is(equalTo("12331")));
+        Assert.assertThat(output.get(0).get("number"), is(equalTo("2018")));
+        Assert.assertThat(output.get(0).get("customer_id"), is(equalTo("8")));
+        Assert.assertThat(output.get(0).get("street1"), is(equalTo("13")));
+        Assert.assertThat(output.get(0).get("street2"), is(equalTo("Wikinews")));
+        Assert.assertThat(output.get(0).get("zip_code"), is(equalTo("English")));
+        Assert.assertThat(output.get(0).get("city"), is(equalTo("Spanish football: Sevilla signs Aleix Vidal from FC Barcelona")));
+        Assert.assertThat(output.get(0).get("country"), is(equalTo("12331")));
+        Assert.assertThat(output.get(0).get("created_at"), is(equalTo("12331")));
+        Assert.assertThat(output.get(0).get("updated_at"), is(equalTo("12331")));
     }
 
     @Test
@@ -55,7 +58,7 @@ public class TemplatePipelineTest {
 
         List<String> input = new ArrayList<>();
 
-        input.add("year,month,day,wikimedia_project,language,title,views");
+        input.add("number,customer_id,street1,street2,zip_code,city,country, created_at, updated_at");
 
         List<TableRow> output = fnTester.processBundle(input);
 
