0d880a6095a758073ceadea764c1d52bbad50fae
AURORE <aurore.pauchet@decathlon.com>
2021-01-24 19:27:19 +0100

    first test

--- src/main/java/org/polleyg/TemplatePipeline.java
+++ src/main/java/org/polleyg/TemplatePipeline.java
@@ -38,8 +38,8 @@ public class TemplatePipeline {
         PipelineOptionsFactory.register(TemplateOptions.class);
         TemplateOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(TemplateOptions.class);
         Pipeline pipeline = Pipeline.create(options);
-        //pipeline.apply("READ", TextIO.read().from(options.getInputFile()))
-        pipeline.apply("READ", TextIO.read().from("gs://dkt-us-ldp-baptiste-test/webhookShopify-05_01_2021_10_11_36.json"))
+        pipeline.apply("READ", TextIO.read().from(options.getInputFile()))
+        //pipeline.apply("READ", TextIO.read().from("gs://dkt-us-ldp-baptiste-test/webhookShopify-05_01_2021_10_11_36.json"))
 
                 .apply("TRANSFORM", ParDo.of(new WikiParDo()))
                 .apply("WRITE", BigQueryIO.writeTableRows()
