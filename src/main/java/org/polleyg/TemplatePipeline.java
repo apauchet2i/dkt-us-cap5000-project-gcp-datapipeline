fefab8cd3183ecab8cbb4a7021417e9cd8c174ba
AURORE <aurore.pauchet@decathlon.com>
2021-01-24 19:24:07 +0100

    first test

--- src/main/java/org/polleyg/TemplatePipeline.java
+++ src/main/java/org/polleyg/TemplatePipeline.java
@@ -6,12 +6,10 @@ import com.google.api.services.bigquery.model.TableSchema;
 import org.apache.beam.sdk.coders.Coder;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.io.gcp.bigquery.TableRowJsonCoder;
-import org.json.*;
 
 import java.io.ByteArrayInputStream;
 
 import org.json.simple.parser.JSONParser;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.Pipeline;
@@ -40,50 +38,10 @@ public class TemplatePipeline {
         PipelineOptionsFactory.register(TemplateOptions.class);
         TemplateOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(TemplateOptions.class);
         Pipeline pipeline = Pipeline.create(options);
-        pipeline.apply("READ", TextIO.read().from(options.getInputFile()))
-        //pipeline.apply("READ", TextIO.read().from("gs://dkt-us-ldp-baptiste-test/webhookShopify-05_01_2021_10_11_36.json"))
-                .apply(
-                        "JSONtoData",                     // the transform name
-                        ParDo.of(new DoFn<String, TableRow>() {    // a DoFn as an anonymous inner class instance
-                            @ProcessElement
-                            public void processElement(ProcessContext c) {
-                                JSONParser parser = new JSONParser();
-                                System.out.println(c.element().getClass());
-                                System.out.println(c.element());
-                                try {
-                                    Object obj = parser.parse(c.element());
-                                    JSONObject jsonObject = (JSONObject) obj;
-                                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("Y-m-d'T'H:MM:SS", Locale.getDefault());
-                                    LocalDateTime now = LocalDateTime.now();
-                                    String timeStampNow = dtf.format(now);
+        //pipeline.apply("READ", TextIO.read().from(options.getInputFile()))
+        pipeline.apply("READ", TextIO.read().from("gs://dkt-us-ldp-baptiste-test/webhookShopify-05_01_2021_10_11_36.json"))
 
-
-                                    JSONObject customer = (JSONObject) jsonObject.get("customer");
-                                    JSONObject shippingAddress = (JSONObject) jsonObject.get("shipping_address");
-                                    System.out.println(customer.get("id"));
-                                    System.out.println(customer.get("id").getClass());
-
-                                    Map<String, String> map = new HashMap<>();
-                                    map.put("number", (String) jsonObject.get("name"));
-                                    map.put("customer_id", String.valueOf(customer.get("id")));
-                                    map.put("street1", (String) shippingAddress.get("address1"));
-                                    map.put("street2", (String) shippingAddress.get("address2"));
-                                    map.put("zip_code", (String) shippingAddress.get("zip"));
-                                    map.put("city", (String) shippingAddress.get("city"));
-                                    map.put("country", (String) shippingAddress.get("country"));
-                                    map.put("created_at", ((String) jsonObject.get("created_at")).substring(0,((String) jsonObject.get("created_at")).length()-6));
-                                    map.put("updated_at", timeStampNow);
-                                    JSONObject jsonToBigQuery = new JSONObject(map);
-                                    System.out.println(jsonToBigQuery);
-                                    c.output(convertJsonToTableRow(String.valueOf(jsonToBigQuery)));
-
-                                }catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-
-                           }
-                        }))
-                //.apply("TRANSFORM", ParDo.of(new WikiParDo()));
+                .apply("TRANSFORM", ParDo.of(new WikiParDo()))
                 .apply("WRITE", BigQueryIO.writeTableRows()
                         .to(String.format("%s:dkt_us_test_cap5000.orders", options.getProject()))
                         .withCreateDisposition(CREATE_IF_NEEDED)
@@ -94,29 +52,25 @@ public class TemplatePipeline {
 
     private static TableSchema getTableSchemaOrder() {
         List<TableFieldSchema> fields = new ArrayList<>();
-        fields.add(new TableFieldSchema().setName("number").setType("STRING"));
-        fields.add(new TableFieldSchema().setName("customer_id").setType("STRING"));
-        fields.add(new TableFieldSchema().setName("street1").setType("STRING"));
-        fields.add(new TableFieldSchema().setName("street2").setType("STRING"));
-        fields.add(new TableFieldSchema().setName("zip_code").setType("STRING"));
-        fields.add(new TableFieldSchema().setName("city").setType("STRING"));
-        fields.add(new TableFieldSchema().setName("country").setType("STRING"));
-        fields.add(new TableFieldSchema().setName("created_at").setType("DATETIME"));
-        fields.add(new TableFieldSchema().setName("updated_at").setType("DATETIME"));
+        fields.add(new TableFieldSchema().setName("number").setType("STRING").setMode("REQUIRED"));
+        fields.add(new TableFieldSchema().setName("customer_id").setType("INTEGER").setMode("REQUIRED"));
+        fields.add(new TableFieldSchema().setName("street1").setType("STRING").setMode("REQUIRED"));
+        fields.add(new TableFieldSchema().setName("street2").setType("STRING").setMode("NULLABLE"));
+        fields.add(new TableFieldSchema().setName("zip_code").setType("STRING").setMode("REQUIRED"));
+        fields.add(new TableFieldSchema().setName("city").setType("STRING").setMode("REQUIRED"));
+        fields.add(new TableFieldSchema().setName("country").setType("STRING").setMode("REQUIRED"));
+        fields.add(new TableFieldSchema().setName("created_at").setType("DATETIME").setMode("REQUIRED"));
+        fields.add(new TableFieldSchema().setName("updated_at").setType("DATETIME").setMode("REQUIRED"));
         return new TableSchema().setFields(fields);
     }
 
     public static TableRow convertJsonToTableRow(String json) {
         TableRow row;
-        // Parse the JSON into a {@link TableRow} object.
-        try (InputStream inputStream =
-                     new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8))) {
+        try (InputStream inputStream = new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8))) {
             row = TableRowJsonCoder.of().decode(inputStream, Coder.Context.OUTER);
-
         } catch (IOException e) {
             throw new RuntimeException("Failed to serialize json to table row: " + json, e);
         }
-
         return row;
     }
 
@@ -126,21 +80,43 @@ public class TemplatePipeline {
 
         void setInputFile(ValueProvider<String> value);
     }
-
     public static class WikiParDo extends DoFn<String, TableRow> {
-        public static final String HEADER = "number,customer_id,street1,street2,zip_code,city,country,created_at,updated_at";
 
         @ProcessElement
         public void processElement(ProcessContext c) throws Exception {
-            if (c.element().equalsIgnoreCase(HEADER)) return;
-            String[] split = c.element().split(",");
-            if (split.length > 9) return;
-            TableRow row = new TableRow();
-            for (int i = 0; i < split.length; i++) {
-                TableFieldSchema col = getTableSchemaOrder().getFields().get(i);
-                row.set(col.getName(), split[i]);
+            JSONParser parser = new JSONParser();
+            System.out.println(c.element().getClass());
+            System.out.println(c.element());
+            Object obj = parser.parse(c.element());
+            JSONObject jsonObject = (JSONObject) obj;
+
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("YYYY-MM-dd'T'H:mm:ss", Locale.getDefault());
+            LocalDateTime now = LocalDateTime.now();
+            String timeStampNow = dtf.format(now);
+
+            JSONObject customer = (JSONObject) jsonObject.get("customer");
+            JSONObject shippingAddress = (JSONObject) jsonObject.get("shipping_address");
+            System.out.println(customer.get("id"));
+            System.out.println(customer.get("id").getClass());
+
+            Map<String, Object> map = new HashMap<>();
+            map.put("number", jsonObject.get("name"));
+            map.put("customer_id", String.valueOf(customer.get("id")));
+            map.put("street1", shippingAddress.get("address1"));
+            if(shippingAddress.get("address2") != null) {
+                map.put("street2", shippingAddress.get("address2"));
             }
-            c.output(row);
+            map.put("zip_code", shippingAddress.get("zip"));
+            map.put("city", shippingAddress.get("city"));
+            map.put("country", shippingAddress.get("country"));
+            map.put("created_at", ((String) jsonObject.get("created_at")).substring(0,((String) jsonObject.get("created_at")).length()-6));
+            map.put("updated_at", timeStampNow);
+            JSONObject jsonToBigQuery = new JSONObject(map);
+            System.out.println(jsonToBigQuery);
+            TableRow tableRow = convertJsonToTableRow(String.valueOf(jsonToBigQuery));
+            System.out.println(tableRow);
+            c.output(tableRow);
+
         }
     }
 }
