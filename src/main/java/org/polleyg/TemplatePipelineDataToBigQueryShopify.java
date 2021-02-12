ea398e129a196bba07557b3567a63aea872307fd
AURORE <aurore.pauchet@decathlon.com>
2021-02-12 15:40:51 +0100

    change to pub sub

--- src/main/java/org/polleyg/TemplatePipelineDataToBigQueryShopify.java
+++ src/main/java/org/polleyg/TemplatePipelineDataToBigQueryShopify.java
@@ -174,7 +174,7 @@ public class TemplatePipelineDataToBigQueryShopify {
                         .withWriteDisposition(WRITE_APPEND)
                         .withSchema(getTableSchemaOrderErrors()));
 
-        pipeline.run().waitUntilFinish();
+        pipeline.run();
     }
 
 
